77e820a13248ca168710f9a282fd502036c0fa7d
Srijan10011 <watckfreetv22@gmail.com>
2025-08-24 10:47:30 +0545

    CHeckout lcoation mobile check

--- Checkout.tsx
+++ Checkout.tsx
@@ -116,9 +116,29 @@ export default function Checkout({ cart, setCurrentPage, session, clearCart }: C
 
   const getLocation = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLocation(`${position.coords.latitude}, ${position.coords.longitude}`);
-      });
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation(`${position.coords.latitude}, ${position.coords.longitude}`);
+        },
+        (error) => {
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              alert("You denied the request for Geolocation.");
+              break;
+            case error.POSITION_UNAVAILABLE:
+              alert("Location information is unavailable.");
+              break;
+            case error.TIMEOUT:
+              alert("The request to get user location timed out.");
+              break;
+            default:
+              alert("An unknown error occurred.");
+              break;
+          }
+        }
+      );
+    } else {
+      alert("Geolocation is not supported by this browser.");
     }
   };
 
